80d0287bbf6a0b16d15044c392b902edbe918f81
Felix-Chang <felix_chang365@hotmail.com>
2025-06-06 17:31:55 -0400

    june 6th

--- src/App.js
+++ src/App.js
@@ -33,13 +33,46 @@ function App() {
         });
     };
 
+    const handleRemoveTask = (taskIndex) => {
+        setTasks((prev) => {
+            return prev.filter((taskObject, index) => index !== taskIndex);
+        });
+    };
+
+    const handleRenameTask = (index, newName) => {
+        setTasks((prev) => {
+            const newTasks = [...prev];
+            newTasks[index].taskName = newName;
+            return newTasks;
+        });
+    };
+
+    const numberComplete = tasks.filter((t) => t.done).length;
+    const numberTotal = tasks.length;
+
+    const getMessage = () => {
+        const percentage = (numberComplete / numberTotal) * 100;
+        if (percentage === 0) {
+            return "Try to do at least one task";
+        } else if (percentage === 100) {
+            return "Nice job for today!";
+        }
+        return "Keep working at it...";
+    };
+
     return (
         <main>
+            <h1 className="count">
+                {numberComplete}/{numberTotal} Complete
+            </h1>
+            <h2 className="message">{getMessage()}</h2>
             <TaskForm onAdd={handleAddTask} />
             {tasks.map((task, index) => (
                 <Task
                     {...task}
                     onToggle={(done) => handleUpdateTask(index, done)}
+                    onTrash={() => handleRemoveTask(index)}
+                    onRename={(newName) => handleRenameTask(index, newName)}
                 />
             ))}
         </main>
--- src/Task.js
+++ src/Task.js
@@ -1,10 +1,40 @@
+import { useState } from "react";
 import Checkbox from "./Checkbox";
+import { FaTrash } from "react-icons/fa";
 
-export default function Task({ taskName, done, onToggle }) {
+export default function Task({ taskName, done, onToggle, onTrash, onRename }) {
+    const [editMode, setEditMode] = useState(false);
+
     return (
         <div className={"task" + (done ? "done" : "not-done")}>
             <Checkbox checked={done} onClick={() => onToggle(!done)} />
-            {taskName}
+            {!editMode && (
+                <div
+                    className="task-label"
+                    onClick={() => setEditMode((prev) => !prev)}
+                >
+                    {taskName}
+                </div>
+            )}
+            {editMode && (
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        setEditMode(false);
+                    }}
+                >
+                    <input
+                        type="text"
+                        value={taskName}
+                        onChange={(e) => onRename(e.target.value)}
+                        className="rename-textbox"
+                    />
+                </form>
+            )}
+
+            <button className="trash" onClick={onTrash}>
+                <FaTrash />
+            </button>
         </div>
     );
 }
